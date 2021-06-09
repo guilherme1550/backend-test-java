d5d83716a901950318a19b7952cf6d54b9e91bce
Guilherme Bento <guilherme1550@hotmail.com>
2021-06-09 14:16:15 -0300

    Commit Inicial

---
+++ src/test/java/com/guilherme1550/apiestacionamento/ApiEstacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme1550.apiestacionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEstacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
