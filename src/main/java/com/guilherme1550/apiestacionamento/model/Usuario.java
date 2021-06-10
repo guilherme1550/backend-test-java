4fc21c0a6bfa24f06d4eb5f3d6e506adb4db296f
Guilherme Bento <guilherme1550@hotmail.com>
2021-06-10 12:06:52 -0300

    feat: Model Usuario

---
+++ src/main/java/com/guilherme1550/apiestacionamento/model/Usuario.java
@@ -0,0 +1,74 @@
+package com.guilherme1550.apiestacionamento.model;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	@Column(name = "id", updatable = false, nullable = false)
+	private UUID id;
+	
+	@Column(name = "cpf", updatable = true, nullable = false)
+	private String cpf;
+	
+	@Column(name = "email", updatable = true, nullable = false)
+	private String email;
+	
+	@Column(name = "senha", updatable = true, nullable = false)
+	private String senha;
+	
+	@ManyToOne
+	private Empresa empresa;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+	
+}
