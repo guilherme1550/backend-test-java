4fa856ff032381563345651f8fbe838a44ec3474
Guilherme Bento <guilherme1550@hotmail.com>
2021-06-10 12:03:40 -0300

    feat: Model TelefoneEmpresa

---
+++ src/main/java/com/guilherme1550/apiestacionamento/model/TelefoneEmpresa.java
@@ -0,0 +1,52 @@
+package com.guilherme1550.apiestacionamento.model;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class TelefoneEmpresa {
+	
+	@Id
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	@Column(name = "id", updatable = false, nullable = false)
+	private UUID id;
+	
+	@Column(name = "numero", updatable = true, nullable = false)
+	private String numero;
+	
+	@ManyToOne
+	private Empresa empresa;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+	
+}
