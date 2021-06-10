fd7a0f0fc7701a7a459937d0ad61d2013e42609e
Guilherme Bento <guilherme1550@hotmail.com>
2021-06-10 11:55:05 -0300

    refactor: Criado métodos getters e setters

--- src/main/java/com/guilherme1550/apiestacionamento/model/Empresa.java
+++ src/main/java/com/guilherme1550/apiestacionamento/model/Empresa.java
@@ -29,4 +29,44 @@ public class Empresa {
 	
 	@Column(name = "endereco", updatable = true, nullable = false)
 	private String endereco;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
 }
