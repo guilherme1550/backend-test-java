84d79cb29361d99d9606001bb3a4bd6bfa0c738e
guilherme <guilherme1550@hotmail.com>
2021-08-09 10:47:19 -0300

    refactor: Listagem de Empresa

--- src/main/java/com/guilherme1550/apiestacionamento/controller/dto/UsuarioEmpresaDto.java
+++ src/main/java/com/guilherme1550/apiestacionamento/controller/dto/UsuarioEmpresaDto.java
@@ -6,14 +6,20 @@ import com.guilherme1550.apiestacionamento.model.Perfil;
 import com.guilherme1550.apiestacionamento.model.UsuarioEmpresa;
 
 public class UsuarioEmpresaDto {
+	private String idUsuario;
 	private String email;
 	private List<Perfil> perfil;
 	
 	public UsuarioEmpresaDto(UsuarioEmpresa usuarioEmpresa) {
+		this.idUsuario = usuarioEmpresa.getId();
 		this.email = usuarioEmpresa.getEmail();
 		this.perfil = usuarioEmpresa.getPerfil();
 	}
 
+	public String getIdUsuario() {
+		return idUsuario;
+	}
+	
 	public String getEmail() {
 		return email;
 	}
