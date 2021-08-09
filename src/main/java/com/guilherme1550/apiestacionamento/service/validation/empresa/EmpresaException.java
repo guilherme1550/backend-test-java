853efdc6001c88d8c3f699cf826b8ebd684e512a
guilherme <guilherme1550@hotmail.com>
2021-08-09 11:35:22 -0300

    refactor: Listagem de Empresas

---
+++ src/main/java/com/guilherme1550/apiestacionamento/service/validation/empresa/EmpresaException.java
@@ -0,0 +1,11 @@
+package com.guilherme1550.apiestacionamento.service.validation.empresa;
+
+public class EmpresaException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public EmpresaException(String mensagem) {
+		super(mensagem);
+	}
+
+}
