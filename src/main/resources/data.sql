e9362d31d9680c71356efcbe8d829b076cacba99
guilherme <guilherme1550@hotmail.com>
2021-08-13 16:16:24 -0300

    feature: Controle de acesso por Perfil

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,2 @@
-INSERT INTO estacionamento(id, nome, cnpj, email, senha) VALUES(1, 'Estacione Aqui', '11222333000112', 'estacionamento@email.com', '123456');
-
-INSERT INTO perfil(id, nome) VALUES('8394b262-82ac-4725-8827-e06ea2f18458', 'USUARIO_EMPRESA');
-INSERT INTO perfil(id, nome) VALUES('19f5c333-4cbb-4239-afaa-62184323dd3c', 'USUARIO_TESTE');
+INSERT INTO perfil(id, nome) VALUES('8394b262-82ac-4725-8827-e06ea2f18458', 'ROLE_USUARIO_EMPRESA_ADM');
+INSERT INTO perfil(id, nome) VALUES('19f5c333-4cbb-4239-afaa-62184323dd3c', 'ROLE_USUARIO_EMPRESA');
